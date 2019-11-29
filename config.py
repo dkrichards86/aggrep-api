666d33517f6c8d6dc0e4a3ea5262f6aeb525e1e8
Keith Richards <dkrichards86@gmail.com>
2019-11-28 19:14:25 -0500

    Adds (empty) defaults to configs.

--- config.py
+++ config.py
@@ -14,29 +14,29 @@ class Config:
     TESTING = False
     LOG_LEVEL = "info"
 
-    SECRET_KEY = env.str("SECRET_KEY")
+    SECRET_KEY = env.str("SECRET_KEY", "")
     BCRYPT_LOG_ROUNDS = 13
 
-    SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL", "")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    _CACHE_HOST = env.str("REDIS_URL")
+    _CACHE_HOST = env.str("REDIS_URL", "")
     _CACHE_DB = 0
     CACHE_TYPE = "redis"
     CACHE_REDIS_URL = "{}/{}".format(_CACHE_HOST, _CACHE_DB)
 
-    _CELERY_HOST = env.str("REDIS_URL")
+    _CELERY_HOST = env.str("REDIS_URL", "")
     _CELERY_DB = 1
     _CELERY_URL = "{}/{}".format(_CELERY_HOST, _CELERY_DB)
     CELERY_BROKER_URL = _CELERY_URL
     CELERY_RESULT_BACKEND = _CELERY_URL
 
-    SENDGRID_DEFAULT_FROM = env.str("SENDGRID_DEFAULT_FROM")
-    SENDGRID_API_KEY = env.str("SENDGRID_API_KEY")
+    SENDGRID_DEFAULT_FROM = env.str("SENDGRID_DEFAULT_FROM", "")
+    SENDGRID_API_KEY = env.str("SENDGRID_API_KEY", "")
 
     WTF_CSRF_ENABLED = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=14)
-    UI_URL = env.str("UI_URL")
+    UI_URL = env.str("UI_URL", "")
 
 
 class ProductionConfig(Config):
