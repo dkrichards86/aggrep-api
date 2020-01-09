38c3e88ebed03fa0e33c039417ec04f179830a21
Keith Richards <dkrichards86@gmail.com>
2020-01-08 19:54:15 -0500

    Switches to cosine similarity using Spacy. (#37)
    * Switches to cosine similarity using Spacy.
    
    * Removes defunt test.
    
    * Fixes bad import in CI.
    
    * Lint cleanup.
    
    * Updates test.

--- aggrep/jobs/relate.py
+++ aggrep/jobs/relate.py
@@ -1,16 +1,29 @@
 """Similarity job."""
-from collections import defaultdict
+import re
 from datetime import timedelta
 
+import spacy
 from flask import current_app
 
 from aggrep import db
 from aggrep.jobs.base import Job
 from aggrep.models import Category, Feed, Post, Similarity, SimilarityProcessQueue
-from aggrep.utils import now, overlap
+from aggrep.utils import now
 
-BATCH_SIZE = 100
-THRESHOLD = 0.75
+new_line = re.compile(r"(/\n)")
+ws = re.compile(r"\s+")
+nlp = spacy.load("en_core_web_md")
+
+BATCH_SIZE = 50
+THRESHOLD = 0.85
+
+
+def clean(text):
+    """Normalize text."""
+    text = text.encode("ascii", "ignore").decode("utf-8")
+    text = new_line.sub(" ", text)
+    text = ws.sub(" ", text)
+    return text.strip()
 
 
 class Relater(Job):
@@ -25,12 +38,11 @@ class Relater(Job):
             SimilarityProcessQueue.post.has(Post.feed.has(Feed.category == category))
         ).all()
 
-        return [eq.post for eq in similar_by_category]
+        return [eq.post.id for eq in similar_by_category]
 
-    def set_entity_cache(self, category):
-        """Get entities from recent posts."""
-        delta = now() - timedelta(days=2)
-        entity_cache = defaultdict(set)
+    def set_post_cache(self, category):
+        """Get recent posts in a given category."""
+        delta = now() - timedelta(days=1)
 
         recent_posts = (
             Post.query.filter(Post.published_datetime >= delta)
@@ -38,49 +50,39 @@ class Relater(Job):
             .all()
         )
 
+        self.recent_posts = dict()
         for rp in recent_posts:
-            for e in rp.entities:
-                entity_cache[e.entity].add(rp.id)
-
-        self.entity_cache = entity_cache
+            doc = nlp(clean(rp.title))
+            self.recent_posts[rp.id] = doc
 
     def process_batch(self, batch):
         """Process a batch of posts."""
-        post_ids = []
         new_similarities = 0
+        batch_ids = set([x for x in batch])
 
-        for post in batch:
-            post_ids.append(post.id)
-            entities = [e.entity for e in post.entities]
-            entity_set = set(entities)
+        for pid in batch:
+            try:
+                pdoc = self.recent_posts[pid]
+            except KeyError:
+                continue
 
-            unioned_post_ids = set()
-            for e in entities:
-                cached = self.entity_cache.get(e)
-                if cached is None:
-                    continue
-                unioned_post_ids |= self.entity_cache[e]
-
-            keyworded_posts = Post.query.filter(
-                Post.id.in_(list(unioned_post_ids))
-            ).all()
-
-            for rp in keyworded_posts:
-                if rp.id == post.id:
+            for rpid, rpdoc in self.recent_posts.items():
+                if pid == rpid:
                     continue
 
-                related_entity_set = set([e.entity for e in rp.entities])
+                if rpid in batch_ids:
+                    continue
 
-                score = overlap(entity_set, related_entity_set)
+                score = pdoc.similarity(rpdoc)
                 if score >= THRESHOLD:
-                    s_to_r = Similarity(source_id=post.id, related_id=rp.id)
+                    s_to_r = Similarity(source_id=pid, related_id=rpid)
                     db.session.add(s_to_r)
-                    r_to_s = Similarity(related_id=post.id, source_id=rp.id)
+                    r_to_s = Similarity(related_id=pid, source_id=rpid)
                     db.session.add(r_to_s)
                     new_similarities += 2
 
         SimilarityProcessQueue.query.filter(
-            SimilarityProcessQueue.post_id.in_(post_ids)
+            SimilarityProcessQueue.post_id.in_(list(batch_ids))
         ).delete(synchronize_session="fetch")
 
         try:
@@ -103,7 +105,6 @@ class Relater(Job):
             else:
                 self.lock.remove()
 
-        self.lock.create()
         similar_queue_count = SimilarityProcessQueue.query.count()
         if similar_queue_count == 0:
             current_app.logger.info(
@@ -111,6 +112,7 @@ class Relater(Job):
             )
             return
 
+        self.lock.create()
         current_app.logger.info(
             "Processing {} posts in similarity processing queue.".format(
                 similar_queue_count
@@ -123,7 +125,7 @@ class Relater(Job):
             if len(enqueued_posts) == 0:
                 continue
 
-            self.set_entity_cache(category)
+            self.set_post_cache(category)
             start = 0
             while start < len(enqueued_posts):
                 if not self.lock.is_locked() or self.lock.is_expired():
